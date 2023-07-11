91311f17106f5bd68cbc508b41cfb033e856df9d
VamshiNakirikanti <76814440+VamshiNakirikanti@users.noreply.github.com>
2023-07-11 19:00:31 +0530

    Add files via upload
    Adding all files

---
+++ decompressor.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+public class decompressor {
+    public static void method(File file) throws IOException {
+        String fileDirectory= file.getParent();
+        FileInputStream fis= new FileInputStream(file);
+        GZIPInputStream gzipIS= new GZIPInputStream(fis);
+        FileOutputStream fos=new  FileOutputStream(fileDirectory+"/Decompressedfilecheck");
+
+        byte[] buffer=new byte[1024];
+
+        int len;
+
+        while((len=gzipIS.read(buffer)) !=-1){
+            System.out.println("length of file"+len);
+            fos.write(buffer, 0,len);
+        }
+        gzipIS.close();
+        fis.close();
+        fos.close();
+    }
+}
